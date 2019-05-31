cf592c2e27b90bc8a881af235bd41fe3a9298765
Oluwajoba Adegboye <oluwajobaadegboye@hotmail.com>
2019-05-30 21:16:27 -0500

    register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,53 +4,61 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerForm: FormGroup;
-  loading = false;
-  submitted = false;
+    registerForm: FormGroup;
+    loading = false;
+    submitted = false;
 
-  constructor(
-      private formBuilder: FormBuilder,
-      private router: Router,
-      // private userService: UserService,
-      // private alertService: AlertService
-      ) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        // private userService: UserService,
+        // private alertService: AlertService
+    ) { }
 
-  ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-          firstName: ['', Validators.required],
-          lastName: ['', Validators.required],
-          username: ['', Validators.required],
-          password: ['', [Validators.required, Validators.minLength(6)]]
-      });
-  }
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            username: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            phoneNumber: ['', [Validators.required, Validators.pattern("[0-9]{0-10}")]],
+            email: ['', [Validators.required, Validators.email]],
+            state: ['', Validators.required],
+            describes: [''],
+            diet: [''],
+            emergencyContact: [''],
+            member: ['']
+        });
+    }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
 
-  onSubmit() {
-      this.submitted = true;
+    onSubmit() {
+        this.submitted = true;
 
-      // stop here if form is invalid
-      if (this.registerForm.invalid) {
-          return;
-      }
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
 
-      this.loading = true;
-      // this.userService.register(this.registerForm.value)
-      //     .pipe(first())
-      //     .subscribe(
-      //         data => {
-      //             this.alertService.success('Registration successful', true);
-      //             this.router.navigate(['/login']);
-      //         },
-      //         error => {
-      //             this.alertService.error(error);
-      //             this.loading = false;
-      //         });
-  }
+        console.log('valid form')
+        this.loading = true;
+        // this.userService.register(this.registerForm.value)
+        //     .pipe(first())
+        //     .subscribe(
+        //         data => {
+        //             this.alertService.success('Registration successful', true);
+        //             this.router.navigate(['/login']);
+        //         },
+        //         error => {
+        //             this.alertService.error(error);
+        //             this.loading = false;
+        //         });
+    }
 }
