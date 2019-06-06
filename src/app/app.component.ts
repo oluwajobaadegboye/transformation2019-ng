3de7241ff9266fd104ad316058c57723b1eb03f0
Oluwajoba Adegboye <oluwajobaadegboye@hotmail.com>
2019-06-05 20:14:41 -0500

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AutoLogoutService } from './services/AutoLogoutService';
 
 @Component({
@@ -6,10 +6,13 @@ import { AutoLogoutService } from './services/AutoLogoutService';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Transformation 2019';
 
   constructor(
     private autoLogout: AutoLogoutService
   ) { }
+
+  ngOnInit(){
+  }
 }
